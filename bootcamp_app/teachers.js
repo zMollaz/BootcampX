876a9401e173d784a1b32d5372095c0c8ab2a5a7
A E <aelmollaz@gmail.com>
2022-01-06 05:01:50 +0000

    Add students.js & teachers.js for CL pgsql query

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,28 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+pool.connect(() => {
+  console.log("connected to the db");
+});
+pool.query(
+  `SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+  FROM teachers 
+  JOIN assistance_requests ON teachers.id = teacher_id
+  JOIN students ON students.id = student_id
+  JOIN cohorts ON cohort_id = cohorts.id
+  WHERE cohorts.name LIKE '%${process.argv[2] || 'JUL02'}%'
+  ORDER BY teachers.name;`
+)
+
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.cohort}: ${user.teacher}`)
+  })
+})
+
+.catch(err => console.error('query error', err.stack));
