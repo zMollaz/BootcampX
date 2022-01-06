9eec1a3fa2428f1182b7779d21e10a3db22b9760
A E <aelmollaz@gmail.com>
2022-01-06 06:20:23 +0000

    Add parameterized queries for students.js & teachers.js

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -9,15 +9,19 @@ const pool = new Pool({
 pool.connect(() => {
   console.log("connected to the db");
 });
-pool.query(
-  `SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-  FROM teachers 
-  JOIN assistance_requests ON teachers.id = teacher_id
-  JOIN students ON students.id = student_id
-  JOIN cohorts ON cohort_id = cohorts.id
-  WHERE cohorts.name LIKE '%${process.argv[2] || 'JUL02'}%'
-  ORDER BY teachers.name;`
-)
+
+const queryString = 
+`SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers 
+JOIN assistance_requests ON teachers.id = teacher_id
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name LIKE $1
+ORDER BY teachers.name;`;
+const cohort = process.argv[2] || 'JUL02';
+const values = [`%${cohort}%`];
+
+pool.query(queryString, values)
 
 .then(res => {
   res.rows.forEach(user => {
